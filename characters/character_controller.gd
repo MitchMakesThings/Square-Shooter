94ee5004c4d77b6ae7fa6d77ae2c0e202b196c3f
MitchMakesThings <mitchreidnz@gmail.com>
2025-08-13 21:47:07 +0100

    Facing the direction of movement when moving.

--- characters/character_controller.gd
+++ characters/character_controller.gd
@@ -5,11 +5,9 @@ var movement_path : PackedVector3Array = []
 func _physics_process(delta):
 	if len(movement_path) < 1:
 		return
-	var target_position = movement_path[0]	
+	var target_position = movement_path[0]
 	if ((global_position - target_position).length() < .1):
 		movement_path.remove_at(0)
 		return
 	global_position = global_position.lerp(target_position, .1)
-	#velocity = (target_position - global_position).normalized() * 300 * delta
-	#move_and_slide()
-	
+	look_at(Vector3(target_position.x, global_position.y, target_position.z))
